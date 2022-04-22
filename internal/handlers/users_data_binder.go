ba2f564bc1581c9f14590b9e62283d598914f9fc
Dmytro Postolenko <dmitry.postolenko.ua@gmail.com>
2022-04-22 18:06:34 +0300

    user request data binder, tests

---
+++ internal/handlers/users_data_binder.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+// UserRequest defines model for UserRequest.
+type UserRequest struct {
+	UserName string `json:"userName" validate:"required,min=4,max=100"`
+	Password string `json:"password" validate:"required,min=8,max=100"`
+}
+
+func bindUserData(c echo.Context) (*UserRequest, error) {
+	u := new(UserRequest)
+
+	// Binding data
+	err := c.Bind(u)
+	if err != nil {
+		errMsg := "Error during request body decoding: "
+		return u, handleError(err, errMsg, http.StatusBadRequest)
+	}
+
+	// Validating data
+	v := validator.New()
+	err = v.Struct(u)
+	if err != nil {
+		errMsg := "Minimal username length is 4 symbols, minimal password length is 8 symbols: "
+		for _, e := range err.(validator.ValidationErrors) {
+			log.Println(e)
+		}
+		return u, echo.NewHTTPError(http.StatusBadRequest, errMsg+err.Error())
+	}
+
+	return u, nil
+}
---
+++ internal/handlers/users_data_binder_test.go
@@ -0,0 +1,76 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBindUserData(t *testing.T) {
+	e := echo.New()
+
+	testData := map[string]string{"userName": "my_login22133", "password": "my_password"}
+	testDataM, _ := json.Marshal(testData)
+
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(echo.POST, "/user", bytes.NewReader(testDataM))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	c := e.NewContext(req, rec)
+
+	userRequest, err := bindUserData(c)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, userRequest.Password, testData["password"])
+		assert.Equal(t, userRequest.UserName, testData["userName"])
+	}
+}
+
+func TestBindUserDataInvalidUserName(t *testing.T) {
+	tests := []struct {
+		name     string
+		testData map[string]string
+		err      string
+	}{
+		{
+			name:     "UserName validation",
+			testData: map[string]string{"userName": "my", "password": "my_password"},
+			err:      "UserName validation failed",
+		},
+		{
+			name:     "Password validation",
+			testData: map[string]string{"userName": "my_login22133", "password": "my"},
+			err:      "Password validation failed",
+		},
+		{
+			name:     "Empty file",
+			testData: map[string]string{},
+			err:      "Loaded empty file",
+		},
+	}
+
+	e := echo.New()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			testDataM, _ := json.Marshal(tt.testData)
+
+			rec := httptest.NewRecorder()
+			req := httptest.NewRequest(echo.POST, "/user", bytes.NewReader(testDataM))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+			c := e.NewContext(req, rec)
+
+			_, err := bindUserData(c)
+			if err == nil {
+				t.Fatal(tt.err)
+			}
+		})
+	}
+}
