8681122c7efa37c9545182d0e96020b156eb55f7
Dmytro Postolenko <dmitry.postolenko.ua@gmail.com>
2022-04-23 17:41:01 +0300

    small models fixes for bun

--- internal/models/company.go
+++ internal/models/company.go
@@ -1,7 +1,7 @@
 package models
 
 type Company struct {
-	Id      string `json:"id" bun:"id,pk,autoincrement"`
+	Id      string `json:"id" bun:"id,pk"`
 	Name    string `json:"name"`
 	Code    string `json:"code"`
 	Country string `json:"country"`
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	Id       string `json:"id"`
+	Id       string `json:"id" bun:"id,pk"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
